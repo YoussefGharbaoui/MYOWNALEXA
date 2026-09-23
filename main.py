37f7b1e0e46d2381b2e548e022a0e076f413d138
YoussefGharbaoui <youssef.gharbaoui.tech@gmail.com>
2026-09-23 19:54:46 +0000

    something to update

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
 import speech_recognition as sr
+import pyttsx3
 
-def speech_to_text():
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    """listens to microphone and returns text"""
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+        try:
+            command = recognizer.recognize_google(audio)
+            return command.lower()
+        except sr.UnknownValueError:
+            print(f"The audio has not been recognized")
+            return
+        except sr.RequestError:
+            print("check your internet connection")
+            return
+
+
+def process_command(command):
     
+def main():
+    speak("startedl.")
+    running = True
+
+    while running:
+        user_text = listen()
+
+        if user_text:
+            running = process_command(user_text)
+
+
+if __name__ == "__main__":
+    main()
+
